605b8601799ffea7e770382b184ce5c30b87b906
Diana Tuz <dianatuz12@gmail.com>
2024-12-17 18:34:29 +0200

    add src

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 "use strict";
-//do not delete this import
+
 import "../styles/main.scss";
+
+window.addEventListener("hashchange", () => {
+  if (window.location.hash === "#menu") {
+    document.body.classList.add("body__no-active");
+  } else {
+    document.body.classList.remove("body__no-active");
+  }
+});
+
+// eslint-disable-next-line no-undef
+new Swiper(".slider", {
+  loop: true,
+  spaceBetween: 20,
+  slidesPerView: 1,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".slider__pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+  },
+}).enable();
